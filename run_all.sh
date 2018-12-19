27dfb311c151f1d4eaed5fdb05d58f0f13b6fd7a
catatonic2aucourant <mohsinjackspiro@gmail.com>
2018-12-19 18:35:16 +0530

    removed logging to files from bash script

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-python prepare_train_test_files.py >> prepare_data.log
-python generate_features2.py >> gen_features2.log
-python generate_features3.py >> gen_features3.log
-python model_8.py >> etr_v3.log
-python model_10.py >> lgb_etr_v4.log
-python nn_model_v3.py >> nn_model.log
-python stacker_v4.py >> stacker.log
+python prepare_train_test_files.py
+python generate_features2.py
+python generate_features3.py
+python model_8.py
+python model_10.py
+python nn_model_v3.py
+python stacker_v4.py
